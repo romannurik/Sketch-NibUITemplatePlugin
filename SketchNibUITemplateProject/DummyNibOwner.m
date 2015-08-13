b26b404400e8deb92cb53faf819dc51feac867ab
Roman Nurik <roman@nurik.net>
2015-08-13 01:08:50 -0400

    Initial code dump

---
+++ SketchNibUITemplateProject/DummyNibOwner.m
@@ -0,0 +1,17 @@
+#import <Cocoa/Cocoa.h>
+
+@interface DummyNibOwner : NSObject
+
+
+/* View bindings go here */
+@property IBOutlet NSComboBox *exampleComboBox;
+@property IBOutlet NSButton *exampleButton;
+@property IBOutlet NSButton *exampleDisclosureButton;
+@property IBOutlet NSTextField *exampleLogView;
+/* End of view bindings */
+
+
+@end
+
+@implementation DummyNibOwner
+@end
